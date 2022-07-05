1c44b405d887067e1c79ba245f1981165286fdb2
Malik <97180802+mjones97@users.noreply.github.com>
2022-07-05 17:37:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const convertBtn = document.getElementById('convert-btn')
+let meterFeet = document.getElementById("meter-feet");
+let litersGallons = document.getElementById("liters-gallons");
+let kilogramsPounds = document.getElementById("kilograms-pounds")
+
+convertBtn.addEventListener("click", function() {
+    const value = Number(document.getElementById('unit').value)
+    const meter = (value * (1/3.281)).toFixed(3)
+    const feet = (value * 3.281).toFixed(3)
+    const liter = (value * (1/0.264)).toFixed(3)
+    const gallon = (value * 0.264).toFixed(3)
+    const kilogram = (value * (1/2.204)).toFixed(3)
+    const pound = (value * 2.204).toFixed(3)
+
+    meterFeet.innerHTML = `<h2 class="card-head">Length (Meter/Feet)</h2>
+                            <p>${value} meters = ${feet} feet | ${value} feet = ${meter} meters
+                            </p>`
+
+    litersGallons.innerHTML = `<h2 class="card-head">Volume (Liters/Gallons)</h2>
+                                <p>${value} liters = ${gallon} gallons | ${value} gallons = ${liter} liters
+                                </p>`
+
+    kilogramsPounds.innerHTML = `<h2 class="card-head">Mass (Kilograms/Pounds)</h2>
+                                <p>${value} kilograms = ${pound} pounds | ${value} pounds = ${kilogram} kilograms
+                                </p>`
+})
